9120611333b71feadf579f21ebd467479d02a842
Shahrukh <65569455+shahruhan@users.noreply.github.com>
2024-03-08 10:36:28 +0500

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 // for atlas connection online with password"
 
-const DB = 'mongodb+srv://shahruhan:ruhi2324@cluster0.ias3lfx.mongodb.net/airService?retryWrites=true&w=majority';
+const DB = 'mongodb+srv://shahruhan:ruhi0077@cluster0.ias3lfx.mongodb.net/airService?retryWrites=true&w=majority';
 
 // const DB = process.env.DATABASE;
 
