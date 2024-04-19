35a8fb532615ebd965ca5e14a5e439b0ae6b9811
Shahrukh <65569455+shahruhan@users.noreply.github.com>
2024-04-19 09:50:29 +0500

    Update auth.js

--- server/router/auth.js
+++ server/router/auth.js
@@ -240,6 +240,9 @@ const storage = multer.diskStorage({
   }
 })
 
+const destinationPath = __dirname '../users'; // specify your destination path
+    console.log('Destination Path:', destinationPath);
+
 const upload = multer({
   storage : storage
 })
